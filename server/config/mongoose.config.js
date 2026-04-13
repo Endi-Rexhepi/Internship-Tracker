c2d8656b01118cbe6a0dc09bcf7e55f5c991048e
Endi Rexhepi <endirexhepi5@gmail.com>
2026-04-13 15:24:35 +0200

    Add dotenv config and fix MONGODB_URI variable name

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log("MongoDB connection error:", err));
